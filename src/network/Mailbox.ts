e99e6647a7c7f82f0248b379c0367242a8bda571
Aleksej Kazmin <prSquirrel@users.noreply.github.com>
2019-05-19 20:19:01 +0300

    Move causal concerns to CausalMailbox

--- src/network/Mailbox.ts
+++ src/network/Mailbox.ts
@@ -1,5 +1,3 @@
-import FastPriorityQueue from 'fastpriorityqueue';
-import { VClock } from './VClock';
 import { client, Client, ClientEvents, PeerSyncContext } from './Client';
 import { Message, Operation, Operation_Timestamp, SyncMessage } from './schema/schema';
 import EventEmitter from 'nanobus';
@@ -14,29 +12,15 @@ export enum MailboxEvents {
 }
 
 //TODO: flag of ready
-class Mailbox extends EventEmitter {
+export class Mailbox extends EventEmitter {
   private client: Client;
-
-  private site?: string;
-  private clock?: VClock;
-  private queue: FastPriorityQueue<VClock>;
 
   constructor(client: Client) {
     super();
-    client.on(ClientEvents.ID_ASSIGNED, (id: string) => {
-      this.site = id;
-      this.clock = VClock.create(id);
-    });
     client.on(ClientEvents.DATA, (msg: capnp.Message) => {
       this.handle(msg);
     });
     this.client = client;
-
-    // this.queue = new FastPriorityQueue((msgA, msgB) => {
-    //   const [msgClockA, payloadA] = msgA;
-    //   const [msgClockB, payloadB] = msgB;
-    //   return VClock.lessThanOrEqualPartial(msgClockA)(msgClockB);
-    // });
   }
 
   broadcast(op: Op<string>): void {
@@ -205,50 +189,6 @@ class Mailbox extends EventEmitter {
   private emitSync(ops: Op<string>[]): void {
     this.emit(MailboxEvents.SYNC_RECEIVED, ops);
   }
-
-  // Processes incoming message of format [senderId, clock, payload] in causal order.
-  // If a message is out of order, it is queued.
-  // Returns: an ordered array of message payloads ready to be processed.
-  //          If a message is not yet ready, it will be returned in subsequent invocations.
-  // process(message) {
-  //   const [senderId, jsonMsgClock, payload] = message;
-  //   const msgClock = Util.fromRight(VClock.fromJson(jsonMsgClock));
-
-  //   const jsonCurrentClock = VClock.asJson(this.clock);
-  //   console.log(`Current clock ${jsonCurrentClock}, Message clock ${jsonMsgClock}`);
-
-  //   if (VClock.precedes(this.clock)(msgClock)(senderId)) {
-  //     this.mergeClockWith(msgClock);
-
-  //     if (!this.queue.isEmpty()) {
-  //       //todo: test that multiple messages are dequeued
-  //       const [clock, queuedPayload] = this.queue.poll();
-  //       return [payload, queuedPayload];
-  //     } else {
-  //       return [payload];
-  //     }
-  //   } else {
-  //     this.queue.add([msgClock, payload]);
-  //     return [];
-  //   }
-  // }
-
-  // Increments internal clock and broadcasts message of format [senderId=peerId, clock, payload] using provided onSend.
-  // broadcast(payload, onSend) {
-  //   this.advanceClock();
-
-  //   const jsonClock = VClock.asJson(this.clock);
-  //   const message = [this.peerId, jsonClock, payload];
-  //   onSend(message);
-  // }
-
-  // advanceClock(): void {
-  //   this.clock.increment();
-  // }
-
-  // mergeClockWith(otherClock: VClock): void {
-  //   this.clock.merge(otherClock);
-  // }
 }
 
 export const mailbox = new Mailbox(client);
